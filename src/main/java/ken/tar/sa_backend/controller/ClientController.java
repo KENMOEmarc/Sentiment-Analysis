0b031d9cccbcabd53834c5c5042daba24adaff48
Wrath <kenmarcbertrand@gmail.com>
2026-03-16 12:03:35 +0100

    Update ClientController.java
    Remove the HttpStatus static import.

--- src/main/java/ken/tar/sa_backend/controller/ClientController.java
+++ src/main/java/ken/tar/sa_backend/controller/ClientController.java
@@ -5,11 +5,11 @@ import ken.tar.sa_backend.entity.Client;
 import ken.tar.sa_backend.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus.NO_CONTENT;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequestMapping(value = "/clients")
